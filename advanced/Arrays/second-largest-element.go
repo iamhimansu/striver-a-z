1e50b633649668447b45d8519733a76c1cc163d7
Himanshu <iamhimanshu7102@gmail.com>
2026-03-18 22:49:00 +0530

    refactor secondLargestElement function for improved logic and clarity

--- advanced/Arrays/second-largest-element.go
+++ advanced/Arrays/second-largest-element.go
@@ -1,33 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func secondLargestElement(nums []int) int {
 
-	currentLargest := 0
-	current := 0
-	secondLargest := -1
+	currentLargest := nums[0]
+	secondLargest := math.MinInt
 
-	//[12, 35, 1, 10, 34, 33, 1]
-	for i := 0; i < len(nums)-1; i++ {
+	for i := 0; i < len(nums); i++ {
+		current := nums[i]
 
-		current = nums[i]
-
-		//next largest
 		if current > currentLargest {
 			secondLargest = currentLargest
 			currentLargest = current
 		} else if current > secondLargest && current != currentLargest {
 			secondLargest = current
 		}
+	}
 
+	// no second largest found
+	if secondLargest == math.MinInt {
+		return -1
 	}
 
 	return secondLargest
-
 }
 
 func main() {
-	nums := []int{12, 35, 1, 32, 10, 30, 31, 1}
+	nums := []int{100, -100}
 	fmt.Println(secondLargestElement(nums))
 }
