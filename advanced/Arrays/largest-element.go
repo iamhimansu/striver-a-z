8fb54629b04d57336d857471a98f14f0b9a3a8d5
Himanshu <iamhimanshu7102@gmail.com>
2026-03-22 20:01:18 +0530

    refactor largestElement function to simplify logic and improve readability

--- advanced/Arrays/largest-element.go
+++ advanced/Arrays/largest-element.go
@@ -3,26 +3,17 @@ package main
 import "fmt"
 
 func largestElement(nums []int) int {
-
 	currentLargest := -1
-	current := 0
-	next := current
-
-	//[12, 35, 1, 10, 34, 1]
-	for i := 0; i < len(nums)-1; i++ {
-
-		current = nums[i]
-		next = nums[i+1]
-
-		//next largest
-		if next > currentLargest {
-			currentLargest = next
+	if len(nums) > 0 {
+		currentLargest = nums[0]
+	}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > currentLargest {
+			currentLargest = nums[i]
 		}
-
 	}
 
 	return currentLargest
-
 }
 
 func main() {
