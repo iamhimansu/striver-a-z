c9d88603b15dc9b61fac2a104e120cb87ca81d34
Himanshu <iamhimanshu7102@gmail.com>
2026-04-09 01:16:46 +0530

    refactor move zeroes functions for clarity and consistency

--- advanced/Arrays/Logic Building/move-zeroes-to-end.go
+++ advanced/Arrays/Logic Building/move-zeroes-to-end.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func moveZeroesToEnd(nums []int) []int {
+func moveZeroesToEndWithoutOrder(nums []int) []int {
 	size := len(nums)
 	j := size - 1
 	for i := 0; i < size; i++ {
@@ -15,10 +15,27 @@ func moveZeroesToEnd(nums []int) []int {
 	return nums
 }
 
+func moveZeroesToEnd(nums []int) []int {
+	/** loop and find the 1st non-zero
+	swap it with zero position and swap the zero with non-zero position
+	**/
+	for i, j := 0, 0; i < len(nums); i++ {
+		if j <= i && nums[i] != 0 {
+			nums[j], nums[i] = nums[i], nums[j]
+			j++
+		}
+	}
+
+	return nums
+}
+
 func main() {
 
-	arr := []int{0, 0, 0, 1, 3, -2}
+	arr := []int{1, 0, 8, -9, 0, 0, 1, 3, -2}
+	mztewo := moveZeroesToEndWithoutOrder(arr)
 	mzte := moveZeroesToEnd(arr)
 
+	fmt.Printf("%v", mztewo)
 	fmt.Printf("%v", mzte)
+
 }
